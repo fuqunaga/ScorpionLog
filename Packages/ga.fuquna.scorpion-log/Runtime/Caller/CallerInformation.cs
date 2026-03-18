a3c23ae773b113f38e5e76d798374d9fcb0cd103
fuqunaga <fukhid@gmail.com>
2026-03-18 15:44:20 +0900

    refactor: simplify CallerInformation struct and add IsExternalInit for C# 9.0 support

--- Packages/ga.fuquna.scorpion-log/Runtime/Caller/CallerInformation.cs
+++ Packages/ga.fuquna.scorpion-log/Runtime/Caller/CallerInformation.cs
@@ -1,42 +1,10 @@
-﻿using System;
-using System.IO;
+﻿using System.IO;
 
-namespace ScorpionLog
+namespace ScorpionLog;
+
+public readonly record struct CallerInformation(string FilePath, int LineNumber, string MemberName)
 {
-    public readonly struct CallerInformation : IEquatable<CallerInformation>
-    {
-        public string FilePath { get; }
-        public int LineNumber { get; }
-        public string MemberName { get; }
-        
-        public string FileName => Path.GetFileName(FilePath);
+    public string FileName => Path.GetFileName(FilePath);
 
-        public CallerInformation(string callerFilePath, int callerLineNumber, string memberName)
-        {
-            FilePath = callerFilePath;
-            LineNumber = callerLineNumber;
-            MemberName = memberName;
-        }
-
-        public override string ToString() => $"{MemberName}@{FileName}:{LineNumber}";
-        
-        #region IEquatable
-        
-        public bool Equals(CallerInformation other)
-        {
-            return FilePath == other.FilePath && LineNumber == other.LineNumber && MemberName == other.MemberName;
-        }
-
-        public override bool Equals(object obj)
-        {
-            return obj is CallerInformation other && Equals(other);
-        }
-
-        public override int GetHashCode()
-        {
-            return HashCode.Combine(FilePath, LineNumber, MemberName);
-        }
-        
-        #endregion
-    }
+    public override string ToString() => $"{MemberName}@{FileName}:{LineNumber}";
 }
---
+++ Packages/ga.fuquna.scorpion-log/Runtime/Polyfills/CSharp9/IsExternalInit.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel;
+
+namespace System.Runtime.CompilerServices;
+
+/// <summary>
+/// C# 9.0で追加された init-only セッターを使用するために必要なクラス。
+/// Unity では C# 9.0 がサポートされていないため、同名のクラスを定義して使用する。
+/// record 型や init プロパティを使用する場合に必要となる。
+/// </summary>
+[EditorBrowsable(EditorBrowsableState.Never)]
+internal static class IsExternalInit { }
